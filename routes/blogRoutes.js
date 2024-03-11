572322613f1c8036ce7ddf81f38cfa6a52f66717
Hennadii Borysevych <genuch3@gmail.com>
2024-03-11 23:19:42 +0100

    Feat: adding clean cache function to add new post

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-
-const Blog = mongoose.model('Blog');
-
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const Blog = mongoose.model("Blog");
+const cleanCache = require("../middlewares/cleanCache");
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,8 +12,7 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    
+  app.get("/api/blogs", requireLogin, async (req, res) => {
     // const redis = require('redis');
     // const util = require('util');
     // const redisUrl = 'redis://127.0.0.1:6379';
@@ -23,16 +21,18 @@ module.exports = app => {
 
     // // Do we have amy cached data in redis related to this query?
     // const cachedBlogs = await redisClient.get(req.user.id);
-    
+
     // // if yes we serve it from redis
     // if (cachedBlogs) {
     //   console.log('Serving from redis');
     //   res.send(JSON.parse(cachedBlogs));
     //   return;
     // }
-    
-    // if no we do the query  
-    const blogs = await Blog.find({ _user: req.user.id });
+
+    // if no we do the query
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
     // console.log('Serving from mongo');
     res.send(blogs);
 
@@ -40,7 +40,7 @@ module.exports = app => {
     // redisClient.expire(req.user.id, 300);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
--- services/cache.js
+++ services/cache.js
@@ -5,30 +5,49 @@ const util = require("util");
 const redisUrl = "redis://127.0.0.1:6379";
 const redisClient = redis.createClient(redisUrl);
 
-redisClient.get = util.promisify(redisClient.get);
+redisClient.hget = util.promisify(redisClient.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
+
+
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
     })
   );
   // see if we have a value for 'key' in redis
-  const cacheValue = await redisClient.get(key);
+  const cacheValue = await redisClient.hget(this.hashKey, key);
 
+  // if yes we return the value
   if (cacheValue) {
     console.log("Serving from redis");
     const doc = JSON.parse(cacheValue);
-    return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
   }
-  // if yes we return the value
 
   // if no we execute the query and cache the result
-  
   const result = await exec.apply(this, arguments);
-  
-  redisClient.set(key, JSON.stringify(result));
-  
+  redisClient.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
+
   return result;
 };
+
+module.exports = {
+  clearHash(hashKey) {
+    redisClient.del(JSON.stringify(hashKey));
+  }
+}
